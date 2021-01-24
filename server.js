fd46c42a2d1dd8320a6e9efbd02f5b2aa9970812
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-23 17:43:38 -0800

    added code to server.js file

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const mysql = require('mysql');
-// const inquirer = require('inquirer');
-const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening at localhost:" + PORT);
+});
