14c783551d5a118a716e4d7e137ff73ba4fb9933
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-24 00:37:31 -0800

    added a picture in the images folder and I also modified the style.css file

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ USE burgers_db;
 CREATE TABLE burgers 
 (
   id INT NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255),
+  burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
